cbfb5d78e4cef01a4bd6fa433b36abe07b085d1b
Bruno Simon <simon.bruno.77@gmail.com>
2021-07-07 18:08:03 +0200

    :art: Create base

--- src/script.js
+++ src/script.js
@@ -18,27 +18,6 @@ const scene = new THREE.Scene()
 const gltfLoader = new GLTFLoader()
 
 /**
- * Gameplay
- */
-let ready = false
-const $overlay = document.querySelector('.overlay')
-const $kaijus = document.querySelectorAll('.kaiju')
-
-for(const _$kaiju of $kaijus)
-{
-    _$kaiju.addEventListener('click', () =>
-    {
-        $overlay.classList.add('hidden')
-        loadKaiju(_$kaiju.dataset.name)
-    })
-}
-
-/**
- * Object
- */
-const buildings = []
-
-/**
  * Sizes
  */
 const sizes = {
@@ -71,120 +50,15 @@ camera.position.y = 1
 camera.position.z = 1
 scene.add(camera)
 
-// // Controls
-// const controls = new OrbitControls(camera, canvas)
-// controls.enableDamping = true
-
-/**
- * Physics world
- */
-const world = new OIMO.World({ 
-    // timestep: 1 / 60, 
-    // iterations: 8, 
-    // broadphase: 2, // 1 brute force, 2 sweep and prune, 3 volume tree
-    // worldscale: 1, // scale full world 
-    // random: true,  // randomize sample
-    // info: false,   // calculate statistic or not
-    // gravity: [0, - 9.8, 0] 
-})
-
-/**
- * Sky
- */
-const sky = new Sky()
-sky.material.uniforms.turbidity.value = 10
-sky.material.uniforms.rayleigh.value = 3
-sky.material.uniforms.mieCoefficient.value = 0.005
-sky.material.uniforms.mieDirectionalG.value = 0.7
-
-const phi = THREE.MathUtils.degToRad(90 - 2)
-const theta = THREE.MathUtils.degToRad(- 70)
-
-const spherical = new THREE.Spherical(1, phi, theta)
-const sunPosition = new THREE.Vector3()
-sunPosition.setFromSpherical(spherical)
-
-sky.material.uniforms.sunPosition.value = sunPosition
-sky.scale.setScalar(450000)
-scene.add(sky)
-
-/**
- * City
- */
-const buildingGeometry = new THREE.BoxGeometry(1, 1, 1)
-const buildingMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff })
-const buildingCollapsedMaterial = new THREE.MeshStandardMaterial({ color: 0xff0000 })
-const citySize = 20
-const cityLines = 40
-const cityInterbuilding = citySize / cityLines
-const cityMaxDistance = Math.hypot(citySize * 0.5, citySize * 0.5)
-
-const createBuildings = () =>
-{
-    for(let i = 0; i < cityLines; i++)
-    {
-        for(let j = 0; j < cityLines; j++)
-        {
-            const building = {}
-            building.collapsed = false
-
-            // Info
-            const width = (0.5 + 0.5 * Math.random()) * 0.4
-            const depth = (0.5 + 0.5 * Math.random()) * 0.4
-
-            const xRandomness = Math.random() * cityInterbuilding * 0.3
-            const zRandomness = Math.random() * cityInterbuilding * 0.3
-
-            building.x = i / cityLines * citySize - citySize * 0.5 + xRandomness
-            building.z = j / cityLines * citySize - citySize * 0.5 + zRandomness
-            
-            const distanceToCenter = Math.hypot(building.x, building.z)
-            const distanceRatio = distanceToCenter / cityMaxDistance * 1.5
-            
-            const height = Math.random() * (1 - distanceRatio) * 2
-            building.y = height * 0.5
-            const build = distanceRatio > (1 - Math.pow(1 - Math.random(), 3))
-
-            if(!build)
-            {
-                // Body
-                building.body = world.add({ 
-                    type: 'box',
-                    size: [width, height, depth], // size of shape
-                    pos: [building.x, building.y, building.z],
-                    rot: [0, 0, 0],
-                    move: true,
-                    density: 1,
-                    friction: 1,
-                    restitution: 1.25
-                })
-
-                // Mesh
-                building.mesh = new THREE.Mesh(buildingGeometry, buildingMaterial)
-                building.mesh.scale.set(width, height, depth)
-                building.mesh.receiveShadow = true
-                building.mesh.castShadow = true
-                scene.add(building.mesh)
-
-                // Save
-                buildings.push(building)
-            }
-        }
-    }
-}
-
-createBuildings()
+// Controls
+const orbitControls = new OrbitControls(camera, canvas)
+orbitControls.enableDamping = true
 
 /**
  * Floor
  */
-const floorBody = world.add({
-    size: [citySize * 2, 1, citySize * 2],
-    pos: [0, - 0.5, 0],
-    density: 1
-})
 const floorMesh = new THREE.Mesh(
-    new THREE.CircleGeometry(citySize * 2, 40),
+    new THREE.CircleGeometry(40, 40),
     new THREE.MeshStandardMaterial({ color: 0x111111 })
 )
 floorMesh.receiveShadow = true
@@ -192,57 +66,14 @@ floorMesh.rotation.x = - Math.PI * 0.5
 scene.add(floorMesh)
 
 /**
- * Kaiju
+ * Cube
  */
-const kaijuGroup = new THREE.Group()
-scene.add(kaijuGroup)
-
-let kaijuMixer = null
-let kaijuAction = null
-let kaijuRotation = - 1
-let kaijuSpeed = 0
-
-const kaijuBody = world.add({ 
-    type: 'box',
-    size: [0.8, 0.8, 0.8], // size of shape
-    pos: [citySize * 0.5, 0.4, 0],
-    rot: [0, 0, 0],
-    move: true,
-    density: 20,
-    friction: 0.4,
-    restitution: 1.25
-})
-
-const loadKaiju = (_name) =>
-{
-    gltfLoader.load(
-        `./${_name}/scene.gltf`,
-        (_gltf) =>
-        {
-            const scale = _name === 'godzilla' ? 0.0015 : 0.5
-            _gltf.scene.scale.set(scale, scale, scale)
-            _gltf.scene.position.y = - 0.4
-            _gltf.scene.traverse((_child) =>
-            {
-                if(_child instanceof THREE.Mesh)
-                {
-                    _child.castShadow = true
-                }
-            })
-            kaijuGroup.add(_gltf.scene)
-    
-            // Animation
-            kaijuMixer = new THREE.AnimationMixer(_gltf.scene)
-            kaijuAction = kaijuMixer.clipAction(_gltf.animations[0])
-            // kaijuAction.weight = 0
-            kaijuAction.timeScale = 1.5
-    
-            kaijuAction.play()
-
-            ready = true
-        }
-    )
-}
+const cube = new THREE.Mesh(
+    new THREE.BoxBufferGeometry(0.5, 0.5, 0.5),
+    new THREE.MeshStandardMaterial({ color: 0xffffff })
+)
+cube.position.y = 0.25
+scene.add(cube)
 
 /**
  * Lights
@@ -281,62 +112,6 @@ renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 /**
- * Controls
- */
-const keysDown = {
-    up: false,
-    right: false,
-    down: false,
-    left: false
-}
-
-window.addEventListener('keydown', (_event) =>
-{
-    switch(_event.code)
-    {
-        case 'ArrowUp':
-        case 'KeyW':
-            keysDown.up = true
-            break
-        case 'ArrowRight':
-        case 'KeyD':
-            keysDown.right = true
-            break
-        case 'ArrowDown':
-        case 'KeyS':
-            keysDown.down = true
-            break
-        case 'ArrowLeft':
-        case 'KeyA':
-            keysDown.left = true
-            break
-    }
-})
-
-window.addEventListener('keyup', (_event) =>
-{
-    switch(_event.code)
-    {
-        case 'ArrowUp':
-        case 'KeyW':
-            keysDown.up = false
-            break
-        case 'ArrowRight':
-        case 'KeyD':
-            keysDown.right = false
-            break
-        case 'ArrowDown':
-        case 'KeyS':
-            keysDown.down = false
-            break
-        case 'ArrowLeft':
-        case 'KeyA':
-            keysDown.left = false
-            break
-    }
-})
-
-/**
  * Animate
  */
 const clock = new THREE.Clock()
@@ -347,77 +122,9 @@ const tick = () =>
     const elapsedTime = clock.getElapsedTime()
     const deltaTime = elapsedTime - lastElapsedTime
     lastElapsedTime = elapsedTime
-
-    // Kaiju Controls
-    if(ready)
-    {
-        if(keysDown.right)
-        {
-            kaijuRotation -= 1 * deltaTime
-        }
-        if(keysDown.left)
-        {
-            kaijuRotation += 1 * deltaTime
-        }
-        if(keysDown.up && kaijuSpeed < 1)
-        {
-            const x = Math.sin(kaijuRotation) * 2
-            const z = Math.cos(kaijuRotation) * 2
-            const force = new OIMO.Vec3(x, 0, z)
-            kaijuBody.applyImpulse(kaijuBody.position, force)
-        }
-    }
-
-    // World
-    world.step()
-
-    // Update scene according to world
-    for(const _building of buildings)
-    {
-        _building.mesh.position.copy(_building.body.position)
-        _building.mesh.quaternion.copy(_building.body.quaternion)
-
-        if(!_building.collapsed)
-        {
-            const distanceToOrigin = Math.hypot(_building.x - _building.body.position.x, _building.z - _building.body.position.z)
-
-            if(distanceToOrigin > cityInterbuilding * 0.1)
-            {
-                _building.collapsed = true
-                _building.mesh.material = buildingCollapsedMaterial
-            }
-        }
-    }
-
-    // // Update controls
-    // controls.update()
-
-    // Update kaiju
-    kaijuSpeed = kaijuGroup.position.distanceTo(kaijuBody.position) / deltaTime
-    kaijuGroup.position.copy(kaijuBody.position)
-    kaijuGroup.rotation.y = kaijuRotation
-
-    if(kaijuMixer)
-    {
-        kaijuAction.setEffectiveWeight(kaijuSpeed)
-        // console.log(kaijuAction.weight)
-        kaijuMixer.update(deltaTime)
-    }
-
-    // Update camera
-    const kaijuAngle = Math.atan2(kaijuGroup.position.z, kaijuGroup.position.x)
-    const kaijuDistanceToCenter = Math.hypot(kaijuGroup.position.z, kaijuGroup.position.x)
-    const kaijuDistanceToCenterRatio = (1 - kaijuDistanceToCenter / (citySize * 0.5))
-    const cameraRadius = kaijuDistanceToCenter + 2
-    const cameraY = 0.2 + kaijuDistanceToCenterRatio * 1.5
-    const cameraX = Math.cos(kaijuAngle) * cameraRadius
-    const cameraZ = Math.sin(kaijuAngle) * cameraRadius
-
-    camera.position.x += (cameraX - camera.position.x) * 4 * deltaTime
-    camera.position.y += (cameraY - camera.position.y) * 4 * deltaTime
-    camera.position.z += (cameraZ - camera.position.z) * 4 * deltaTime
-
-    camera.lookAt(scene.position)
+    
+    // Update orbit controls
+    orbitControls.update()
 
     // Render
     renderer.render(scene, camera)
